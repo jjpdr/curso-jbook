81f036ea59adbd9cd391ed2a6be84b5b94c6d290
jjpdr <joaop.marcelino@hotmail.com>
2022-01-20 18:21:41 -0300

    feat: 🎸 improved code execution

--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,13 @@ import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
     const [input, setInput] = useState("");
-    const [code, setCode] = useState(""); //output
 
     const onClick = async () => {
         if (!ref.current) return;
+
+        iframe.current.srcdoc = html;
 
         const result = await ref.current.build({
             entryPoints: ["index.js"],
@@ -23,7 +25,10 @@ const App = () => {
             },
         });
 
-        setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(
+            result.outputFiles[0].text,
+            "*"
+        );
     };
 
     const startService = async () => {
@@ -38,9 +43,23 @@ const App = () => {
     }, []);
 
     const html = `
-    <script>
-        ${code}
-    </script>
+    <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    try {
+                        eval(event.data);
+                    } catch (err) {
+                        const root = document.querySelector('#root');
+                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+                        console.error(err);
+                    }
+                }, false)
+            </script>
+        </body>
+    </html>
     `;
 
     return (
@@ -52,8 +71,12 @@ const App = () => {
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <pre>{code}</pre>
-            <iframe sandbox="allow-scripts" srcDoc={html} />
+            <iframe
+                title="preview"
+                ref={iframe}
+                sandbox="allow-scripts"
+                srcDoc={html}
+            />
         </div>
     );
 };
