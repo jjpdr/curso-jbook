74d62b837fa82aa7774efd6feab33de9fd02fd5e
jjpdr <joaop.marcelino@hotmail.com>
2022-01-20 17:12:23 -0300

    feat: 🎸 code can now be executed inside browser

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
 
 const App = () => {
     const ref = useRef<any>();
@@ -15,7 +16,7 @@ const App = () => {
             entryPoints: ["index.js"],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin()],
+            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
             define: {
                 "process.env.NODE_ENV": '"production"',
                 global: "window",
@@ -28,13 +29,19 @@ const App = () => {
     const startService = async () => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: "/esbuild.wasm",
+            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
         });
     };
 
     useEffect(() => {
         startService();
     }, []);
+
+    const html = `
+    <script>
+        ${code}
+    </script>
+    `;
 
     return (
         <div>
@@ -46,6 +53,7 @@ const App = () => {
                 <button onClick={onClick}>Submit</button>
             </div>
             <pre>{code}</pre>
+            <iframe sandbox="allow-scripts" srcDoc={html} />
         </div>
     );
 };
