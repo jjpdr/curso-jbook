0332fa6ab59e17b43d4984d8a29b418ecda5c37d
jjpdr <joaop.marcelino@hotmail.com>
2022-01-20 20:25:14 -0300

    refactor: 💡 code cleaning and moved plugins to inside bundler

---
+++ src/bundler/index.ts
@@ -0,0 +1,28 @@
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
+import { fetchPlugin } from "./plugins/fetch-plugin";
+
+let service: esbuild.Service;
+
+export default async (rawCode: string) => {
+    //This service only need to be started one single time.
+    if (!service) {
+        service = await esbuild.startService({
+            worker: true,
+            wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+        });
+    }
+
+    const result = await service.build({
+        entryPoints: ["index.js"],
+        bundle: true,
+        write: false,
+        plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+        define: {
+            "process.env.NODE_ENV": '"production"',
+            global: "window",
+        },
+    });
+
+    return result.outputFiles[0].text;
+};
