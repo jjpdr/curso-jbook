50f538731385c249b6b335855dc1df265df8bb22
jjpdr <joaop.marcelino@hotmail.com>
2022-01-22 18:55:30 -0300

    feat: 🎸 added cumulative code execution

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -14,22 +14,34 @@ interface CodeCellProps {
 const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { updateCell, createBundle } = useActions();
     const bundle = useTypedSelector((state: any) => state.bundles[cell.id]);
+    const cumulativeCode = useTypedSelector((state: any) => {
+        const { data, order } = state.cells;
+        const orderedCells = order.map((id: any) => data[id]);
+
+        const cumulativeCode = [];
+        for (let c of orderedCells) {
+            if (c.type === "code") cumulativeCode.push(c.content);
+            if (c.id === cell.id) break;
+        }
+
+        return cumulativeCode;
+    });
 
     useEffect(() => {
         //First time page is loaded
         if (!bundle) {
-            createBundle(cell.id, cell.content);
+            createBundle(cell.id, cumulativeCode.join("\n"));
             return;
         }
 
         const timer = setTimeout(async () => {
-            createBundle(cell.id, cell.content);
+            createBundle(cell.id, cumulativeCode.join("\n"));
         }, 777);
 
         return () => {
             clearTimeout(timer);
         };
-    }, [cell.id, cell.content, createBundle]);
+    }, [cell.id, cumulativeCode.join("\n"), createBundle]);
 
     return (
         <Resizable direction="vertical">
